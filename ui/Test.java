82618623c616ad1c33e271a77286230dda3c4831
Ainsleigh Hill <Ainsleigh@Ainsleighs-Air.hitronhub.home>
2018-09-09 21:41:27 -0700

    Starting P1

--- ui/Test.java
+++ ui/Test.java
@@ -1,4 +1,16 @@
 public class Test {
+    public int addInt(int a, int b){
+        sum = a + b;
+        System.out.println("%d + %d = %d", a, b, sum);
+        return sum
+    }
 
-    
+    public int multInt(int a, int b) {
+        product = a*b;
+        System.out.println("%d * %d = %d", a, b, product);
+        return product
+    }
+    public static void main {
+        System.out.println("This is the main method");
+    }
 }
