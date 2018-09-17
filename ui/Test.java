5aec827b76ee428ef4a30ba31f5f391d874ddfa7
Ainsleigh Hill <Ainsleigh@dhcp-128-189-247-189.ubcsecure.wireless.ubc.ca>
2018-09-17 13:58:27 -0700

    test

--- ui/Test.java
+++ ui/Test.java
@@ -1,16 +1,18 @@
 public class Test {
     public int addInt(int a, int b){
+        int sum;
         sum = a + b;
-        System.out.println("%d + %d = %d", a, b, sum);
-        return sum
+        System.out.printf("%d + %d = %d", a, b, sum);
+        return sum;
     }
 
     public int multInt(int a, int b) {
+        int product;
         product = a*b;
-        System.out.println("%d * %d = %d", a, b, product);
-        return product
+        System.out.printf("%d * %d = %d", a, b, product);
+        return product;
     }
-    public static void main {
+    public static void main(String[] args){
         System.out.println("This is the main method");
     }
 }
