85f9cbb2cca7fd63c0f084063859dfa4df5bfed2
a0y0b <ainsleighhill@gmail.com>
2018-09-24 15:50:30 -0700

    fixed bug ish

--- src/data/DataInput.java
+++ src/data/DataInput.java
@@ -52,10 +52,25 @@ public class DataInput {
     public void readCSV(ArrayList<String> csvList){
         int numFiles = csvList.size();
 
+        File file = new File(csvList.get(0));
+        try{
+            Scanner scanner0 = new Scanner(file);
+            //scanner.useDelimiter(",");
+            String row0 = scanner0.nextLine();
+            String[] rowList0 = row0.split(",");
+            System.out.println(rowList0[0]);
+            flowData flow = new flowData(rowList0.length);
+            System.out.println(flow.size);
+            scanner0.close();}
+            catch (FileNotFoundException fe){
+            fe.printStackTrace();
+                flowData flow = new flowData(1);
+        }
+
         // Read each file in the list
 
         for(int i = 0; i < numFiles; i++){
-            File file = new File(csvList.get(i));
+            file = new File(csvList.get(i));
             try {
                 Scanner scanner = new Scanner(file);
                 //scanner.useDelimiter(",");
@@ -64,15 +79,23 @@ public class DataInput {
                     String row = scanner.nextLine();
                     String[] rowList = row.split(",");
                     System.out.println(rowList[0]);
-                    if(i==0){
-                        flowData flow = new flowData(rowList.length);
-                    }
+
                     for(int j=0; j < rowList.length; j++){
                         System.out.println(rowList.length);
                         float cell = Float.parseFloat(rowList[j]);
+                        System.out.println(flow.size);
                         System.out.println(cell);
-                        System.out.println(flow.data.length);
-                        flow.data[j].add(cell);
+                        if(i==0){
+                            ArrayList<Float> Column = new ArrayList<>();
+                            Column.add(cell);
+
+                            flow.data.set(j, Column);
+                        } else {
+                            ArrayList<Float> Column = flow.data.get(j);
+                            Column.add(cell);
+                            flow.data.set(j, Column);
+                        }
+
                     }
                     System.out.println(scanner.nextLine());
                     System.out.println(scanner.next());
